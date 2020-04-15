1afbdf4c014132cf01a6ce4dfc70f4d059b4d3eb
GusNajera <gvt_a9@hotmail.com>
2020-04-15 13:48:22 -0500

    7th, procfile and graph and mongoose

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const express = require('express');
-// const graphqlHTTP = require('express-graphql');
-// const schema = require('./schema/schema');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 app.use(express.static('public'))
-// app.use(cors());
+app.use(cors());
 
-// const uri = 'mongodb+srv://onceonceonce11:onceonceonce11@nemo-rkcm5.mongodb.net/graphQL?retryWrites=true&w=majority';
-// mongoose.connect(uri,{
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-// }) 
-// .then(() => console.log('connection to db'))
-// .catch( e => console.log(e))
+const uri = 'mongodb+srv://onceonceonce11:onceonceonce11@nemo-rkcm5.mongodb.net/graphQL?retryWrites=true&w=majority';
+mongoose.connect(uri,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}) 
+.then(() => console.log('connection to db'))
+.catch( e => console.log(e))
 
 
-// app.use('/', graphqlHTTP({
-//     schema: schema, // you can also put just schema cause it has the same name:V
-//     // graphiql:true
-// }));
-app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>")
-})
+app.use('/', graphqlHTTP({
+    schema: schema, // you can also put just schema cause it has the same name:V
+    // graphiql:true
+}));
 
 const PORT = process.env.PORT || 4001;
 
